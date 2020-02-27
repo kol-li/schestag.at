4d78d2ff3aef5d4c1f847bc0c50f23c57d7870b5
Michael Schestag <michael@tasktilldone.com>
2020-02-27 14:06:45 +0100

    Imprint update

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,13 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images/`
+      }
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
--- src/pages/imprint.js
+++ src/pages/imprint.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-const ImprintPage = () => (
+const ImprintPage = ({ data }) => (
   <Layout>
     <div id="main">
       <ul className="labeled-icons">
@@ -25,9 +26,27 @@ const ImprintPage = () => (
       </ul>
       <p>Unternehmensgegenstand: Entwicklung sowie Wartung und Erweiterung von Software und webbasierten Applikationen</p>
       <p>UID-Nr.: ATU69989916</p>
-      <p><a href="https://is.gd/7liPl8" target="_blank" rel="noopener noreferrer">Mitglied der WKÖ & WKW</a></p>
+      <p>
+        {data.allFile.edges.map(({ node }) => (
+          <a className="ecg" href="https://www.wkoecg.at/Web/Ecg.aspx?FirmaID=0c6b9543-a400-445e-88df-4c6a347bf1d6&_ga=2.173802727.516936350.1582557125-2084495530.1572278246" target="_blank" rel="noopener noreferrer">
+            <img src={node.publicURL} alt="ECG- und Mediengesetz-Link" />
+          </a>
+        ))}
+      </p>
     </div>
   </Layout>
 )
 
 export default ImprintPage
+
+export const query = graphql`
+  query {
+    allFile(filter: {sourceInstanceName: {eq: "images"}, name: {eq: "ecg"}}) {
+      edges {
+        node {
+          publicURL
+        }
+      }
+    }
+  }
+`
