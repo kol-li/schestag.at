ea5751d539fb03aede389eab2bd02892f06dbbcc
Michael Schestag <michael@tasktilldone.com>
2020-03-02 11:56:20 +0100

    Manifest update

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Michael Schestag',
+        short_name: 'schestag.at',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#3E5382',
+        theme_color: '#3E5382',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       }
