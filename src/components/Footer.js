788079c76f4e1fc96d09d68ad0e1ce5766233084
Michael Schestag <michael@tasktilldone.com>
2023-12-22 11:58:59 +0100

    Copyright date update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -42,7 +42,7 @@ class Footer extends React.Component {
                     </ul>
                     <ul className="copyright">
                         <li>
-                            &copy; 2022 Michael Schestag
+                            &copy; 2023 Michael Schestag
                         </li>
                         <li>
                             Design: <a href="https://html5up.net" target="_blank" rel="noopener noreferrer">HTML5 UP</a>
