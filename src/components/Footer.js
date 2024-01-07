d74bf6a7d27e5746fe790b230d23e641baf5e15b
Michael Schestag <michael@tasktilldone.com>
2024-01-07 14:12:48 +0100

    Accessibility improvements

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,37 +6,34 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="location">
-                        Current location: <a href="https://goo.gl/maps/mzB59Fg337YuequBA" target="_blank" rel="noopener noreferrer">Vienna, Austria</a>
+                        <li>
+                            Current location: <a href="https://goo.gl/maps/mzB59Fg337YuequBA" target="_blank" rel="noopener noreferrer">Vienna, Austria</a>
+                        </li>
                     </ul>
                     <ul className="icons">
                         <li>
                             <a href="https://stackoverflow.com/users/3432305/kolli" className="icon" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-stack-overflow"/>
-                                <span className="label">Stack Overflow</span>
+                                <i className="fab fa-stack-overflow" aria-label="Michael Schestag on Stack Overflow"/>
                             </a>
                         </li>
                         <li>
                             <a href="https://www.linkedin.com/in/michaelschestag/" className="icon" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-linkedin-in"/>
-                                <span className="label">LinkedIn</span>
+                                <i className="fab fa-linkedin-in" aria-label="Michael Schestag on LinkedIn"/>
                             </a>
                         </li>
                         <li>
                             <a href="https://twitter.com/kol_li" className="icon" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-twitter"/>
-                                <span className="label">Twitter</span>
+                                <i className="fab fa-twitter" aria-label="Michael Schestag on Twitter"/>
                             </a>
                         </li>
                         <li>
                             <a href="https://www.instagram.com/kol.lee/" className="icon" target="_blank" rel="noopener noreferrer">
-                                <i className="fab fa-instagram"/>
-                                <span className="label">Instagram</span>
+                                <i className="fab fa-instagram" aria-label="Michael Schestag on Instagram"/>
                             </a>
                         </li>
                         <li>
                             <a href="javascript:unCryptHref('nbjmup;dpoubduAtdiftubh/bu');" className="icon">
-                                <i className="fas fa-envelope"/>
-                                <span className="label">Email</span>
+                                <i className="fas fa-envelope" aria-label="Contact Michael Schestag by Email"/>
                             </a>
                         </li>
                     </ul>
